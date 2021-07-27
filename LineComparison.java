9e2bbd8d5f9be236dfd38b1c389e4f30b6b1a25d
sandhiya97 <sandhiya.baburao@gmail.com>
2021-07-27 07:25:50 +0530

    Welcome to Line Comparison Computation Program

---
+++ LineComparison.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class LineComparison {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Line Comparison Computation Program");
+		}
+}
